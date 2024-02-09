b412914df62a00ad84417af6347252aa729caf0e
derryev <derryev@ssfs.org>
2024-02-09 13:56:20 -0500

    Initial Commit

---
+++ repoTesting2/repoTesting2App.swift
@@ -0,0 +1,17 @@
+//
+//  repoTesting2App.swift
+//  repoTesting2
+//
+//  Created by Eva Derryberry on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct repoTesting2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
